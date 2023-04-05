fb6ac7a6d97dc5aa573c9ac4bd24ac7f4d8fa6b9
Mate Granic <mategranic@Mates-Air.home>
2023-04-05 11:30:50 +0200

    Remove all native code
    Removed all native Kotlin code that was done to execute background process on Android platform.

--- PhotoRoute/android/app/src/main/kotlin/com/kingoapps/photo_route/MainActivity.kt
+++ PhotoRoute/android/app/src/main/kotlin/com/kingoapps/photo_route/MainActivity.kt
@@ -23,47 +23,4 @@ import io.flutter.embedding.engine.dart.DartExecutor;
 
 class MainActivity: FlutterActivity() {
 
-    private val CHANNEL = "com.kingoapps.photo_route/background"
-     private var mCallbackDispatcherHandle: Long = 0
-
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        super.configureFlutterEngine(flutterEngine)
-        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
-            // Note: this method is invoked on the main thread.
-            call, result ->
-            if (call.method == "executeBackgroundTask_native") {
-
-                    val args = arrayListOf<Long>();
-                args.addAll(call.arguments());
-                val callBackHandle = args[0];
-                mCallbackDispatcherHandle = callBackHandle;
-
-                val flutterTask : FlutterTask = FlutterTask("ovo je nativ battery parametar", result::success);
-
-                FlutterTask.initializeTask(result::success);
-                FlutterTask.initCtx(this);
-
-                
-                val myData: Data = androidx.work.workDataOf("KEY_X_ARG" to callBackHandle,
-                "KEY_Y_STR" to "string param", "KEY_Z_INT" to 11);
-
-                // todo: mategr
-                // try this https://developer.android.com/guide/background/persistent#kotlin
-                val uploadWorkRequest: WorkRequest =
-                        OneTimeWorkRequestBuilder<UploadWorker>()
-                                .setInputData(myData)
-                                .build()
-
-                WorkManager
-                        .getInstance(this)  // this is Context
-                        .enqueue(uploadWorkRequest)
-
-                // return result to the main app
-                // 27 is just arbitrary number
-                result.success(27)
-            } else {
-                result.notImplemented()
-            }
-        }
-    }
 }
